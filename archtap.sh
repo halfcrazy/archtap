29f66c955bd1d87b167b35d5227c0d51cd2ffb1e
Wu, Xingbo <wuxb45@gmail.com>
2014-07-25 14:35:04 -0400

    Update archtap.sh
    default N

--- archtap.sh
+++ archtap.sh
@@ -4,13 +4,13 @@
 #sudo pacman -Sy
 linuxi=$(pacman -Qu | grep 'linux ')
 if [ -z $linuxi ]; then
-  echo No new Kernel.
+  echo "No new Kernel."
   #exit 0
 else
-  echo New Kernel available.
+  echo "New Kernel available."
 fi
 
-read -p "Build kernel[y/n]?" -n 1 -r
+read -p "Build kernel[y/N]?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]];then
   ABSROOT=. abs core/linux; cd core/linux
@@ -22,8 +22,8 @@ CONFIG_DEBUG_INFO=y
 CONFIG_DEBUG_INFO_REDUCED=n
 ' >> config.x86_64
   makepkg --skipchecksums
-  echo Build done.
-  read -p "Install kernel[y/n]?" -n 1 -r
+  echo "Build done."
+  read -p "Install kernel[y/N]?" -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ ]];then
     pacman -U *.pkg.tar.xz
   fi
